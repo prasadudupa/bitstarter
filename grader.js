6aa4537ff894d1b1d08e5ac8b586bd08debbc61c
Prasad Udupa <prasad.udupa@gmail.com>
2013-07-12 05:29:40 +0000

    updated grader.js for homework

--- grader.js
+++ grader.js
@@ -22,9 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('sys');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,9 +39,31 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(url) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
+    //sys.puts(fs.readFileSync(htmlfile));
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+var cheerioUrl = function(url) {
+  rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    return cheerio.load('');
+  } else {
+    sys.puts(result);
+    return cheerio.load(result);
+  }});
+};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -55,6 +80,28 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksFile) {
+  rest.get(url).on('complete', function(result) {
+  if (result instanceof Error) {
+    sys.puts('Error: ' + result.message);
+    $ = cheerio.load('');
+  } else {
+    //sys.puts(result);
+    $ = cheerio.load(result);
+  }
+    var checks = loadChecks(checksFile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    //return out;
+    var outJson = JSON.stringify(out, null, 4);
+
+    console.log(outJson);
+  });
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +112,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <link_to_web_page>', 'URL to HTML page', null, URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = "";
+    if (program.url == URL_DEFAULT) {
+      checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
+
     console.log(outJson);
+    } else { // if (program.url != URL_DEFAULT)
+      checkJson = checkUrl(program.url, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkUrl = checkUrl;
 }
